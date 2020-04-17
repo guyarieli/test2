3513d1e0ba8437ac73637c087afc55ae61309c0a
root <root@1cfcaf6f9d4b>
2020-04-17 11:37:08 +0000

    Init version

---
+++ src/test/java/com/experitest/auto/IOSDemoTest.java
@@ -0,0 +1,16 @@
+package com.experitest.auto;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import com.experitest.auto.Steps;
+
+@TestInfo(name = "stam", type = TestType.IOS)
+public class IOSDemoTest extends TestBase {
+
+    @Test(groups = "all")
+    public void test() {
+
+    }
+}
